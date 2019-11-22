98bb7d4b5137b7bd8476a493d8d8cee6a51c134c
Stephen <ProbablePattern@users.noreply.github.com>
2019-11-21 20:27:07 -0500

    Create Quantmod_Correlation.R

---
+++ Charts/Quantmod_Correlation.R
@@ -0,0 +1,6 @@
+library(quantmod)
+getSymbols(c("SPY","^DJI"),src="yahoo")
+data=data.frame(SPY[,6],DJI[,6])
+data=as.xts(data,order.by=as.Date(row.names(data),"%Y-%m-%d"))
+c1=rollapply(data,65,cor,by.column=F)
+Correlation=c1[,2]; chartSeries(Correlation)
