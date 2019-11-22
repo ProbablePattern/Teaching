b6ffd30f4e7dca6539ab7863b397561dc6202da4
Stephen <ProbablePattern@users.noreply.github.com>
2019-11-21 20:25:32 -0500

    Create Precious_Metals.R

---
+++ Charts/Precious_Metals.R
@@ -0,0 +1,11 @@
+# Charts Gold prices in USD from Oanda
+require(Quantmod)
+#fetch data
+getSymbols(c("XAU/USD","XPT/USD","XPD/USD","XAG/USD"),src="oanda")
+#setup the plot area, each TA is another graph
+layout(matrix(1:4, nrow=2))
+#charts to be included
+chartSeries(XAUUSD,name="Gold (.oz) in USD", layout=NULL)
+chartSeries(XPTUSD,name="Platinum (.oz) in USD", layout=NULL)
+chartSeries(XPDUSD,name="Palladium (.oz) in USD", layout=NULL)
+chartSeries(XAGUSD,name="Silver (.oz) in USD", layout=NULL)
