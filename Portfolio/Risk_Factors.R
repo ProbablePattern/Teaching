c0e93d43588ac05f9d6ef4adfdb84fc28e66fa4a
ProbablePattern <probablepattern@users.noreply.github.com>
2020-10-19 08:25:21 -0400

    updates to assignment

--- Portfolio/Risk_Factors.R
+++ Portfolio/Risk_Factors.R
@@ -4,20 +4,19 @@ install.packages(c('quantmod','xts'))
 require(quantmod); require(xts)
 
 # Load FF3 data from French's website
-#factors=read.csv(file="C:\\Users\\srrush\\Downloads\\FF3_Daily.CSV",header=TRUE)
-factors=read.csv(file="FF3_Daily.CSV",header=TRUE)
+factors=read.csv(file="C:\\Users\\srrush\\Downloads\\FF3.CSV",header=TRUE)
+#factors=read.csv(file="FF3_Daily.CSV",header=TRUE)
+#factors=FF3
 
 # Look at the beginning of the data
 head(factors)
+colnames(factors)=c("Date","MRP","SMB","HML","RF")
 
-# Stastical summary of the data
+# Statistical summary of the data
 summary(factors)
 
 # If not in decimal format, run this
-#factors[,2:5]=factors[,2:5]/100
-
-# Change the name of the first column to "Date"
-#colnames(factors)[1]='Date'
+factors[,2:5]=factors[,2:5]/100
 
 # Format dates as daily date data
 factors[,'Date']=as.Date(as.character(factors[,'Date']),'%Y%m%d')
@@ -26,7 +25,7 @@ factors[,'Date']=as.Date(as.character(factors[,'Date']),'%Y%m%d')
 factors=na.omit(factors)
 
 # Convert to XTS format for time series
-factors=as.xts(factors[,c('Mkt.RF','SMB','HML','RF')],order.by=factors[,'Date'])
+factors=as.xts(factors[,c('MRP','SMB','HML','RF')],order.by=factors[,'Date'])
 
 # Check the data
 head(factors['2018-01-01/2018-02-01'])
@@ -34,23 +33,27 @@ head(factors['2018-01-01/2018-02-01'])
 #################################################################################
 # Load portfolio returns
 #returns=read.csv(file="C:\\Users\\srrush\\Downloads\\BGSU.csv",header=TRUE)
-returns=read.csv(file="BGSU.csv",header=TRUE)
+#returns=read.csv(file="BGSU.csv",header=TRUE)
+value=Weekly; rm(Weekly)
 
 # Only use first 2 columns: date and return
-#returns=returns[,1:2]
+value=value[,1:2]
+colnames(value)=c('Date','Value')
 
 # Format date as daily date data
-returns[,'Date']=as.Date(as.character(returns[,'Date']),'%m/%d/%Y')
+value[,'Date']=as.Date(as.character(value[,'Date']),'%m/%d/%Y')
 
 # Check the data
-head(returns)
+head(value)
 
 # Remove missing observations
-returns=na.omit(returns)
-returns=as.xts(as.numeric(returns[,2]),order.by=returns[,1])
+value=na.omit(value)
+value=as.xts(as.numeric(value[,2]),order.by=value[,1])
+returns=periodReturn(value,period="weekly")
 
 # Merge factors and portfolio returns
 data=merge.xts(returns,factors)
+colnames(data)[1]="returns"
 
 # Check the data
 head(data)
@@ -63,7 +66,7 @@ data$RP=as.numeric(data$returns-data$RF)
 #data[,'RP']=as.numeric(data[,'returns']-data[,'RF'])
 
 # Define the model and run the regression
-m1=RP~Mkt.RF+SMB+HML; ols=lm(m1,data)
+m1=RP~MRP+SMB+HML; ols=lm(m1,data)
 #m2=RP~Mkt.RF; ols=lm(m2,data)
 
 # Show the regression output
